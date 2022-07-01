303b46399d4e1bda8b2278565a0bb9b5c0a3a918
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-01 02:17:10 +0000

    Fix bugs on C program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase "with proper grammar, but the outcome is apiece of art," followed by a new line
+ * main - prints the phrase with proper grammar,
+ * but the outcome is apiece of art, followed by a new line
  * return 0 if executed successfully and a non-zero otherwise
- **/
+ */
 
 int main(void)
 {
