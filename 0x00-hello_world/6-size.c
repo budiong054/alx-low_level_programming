7ca11b1c12ff629e6b31edd43df3d5098afc59a4
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-01 03:30:15 +0000

    Solve task 6 finally

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of a int: %lu byte(s)\n", sizeof(i));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
