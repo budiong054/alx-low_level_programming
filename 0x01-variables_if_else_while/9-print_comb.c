ee865a851e4355c0aa60f96ae9194f7f0c0c1ce4
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-02 13:32:04 +0000

    Solve task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Return: 0 if successfull, otherwise return 0
+ */
+
+int main(void)
+{
+	char num;
+
+	num = '0';
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
