8c145f45b0145cbfc66f8451ce1498f4e2b7ceb7
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-05 12:42:00 -0700

    Solve task 0 and 1 succesfully

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase follwed by a new line
+ * Return: nothing
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
