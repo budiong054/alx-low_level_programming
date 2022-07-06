07cba3ae3d6bf433ca847df3bf985e5663636bd7
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-06 04:36:13 +0000

    Solve task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @num: number to print its last digit
+ * Return: the last digit
+ */
+
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
