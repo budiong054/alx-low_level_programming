96c7a761134f62345c7d75eea51d483a090b7a20
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-07 07:47:03 +0000

    Make changes to the else if block

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,7 +16,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if ((b > a && a > c) || (b > c && c < a))
+	else if ((b > a && a > c) || (b > c && c > a))
 	{
 		largest = b;
 	}
