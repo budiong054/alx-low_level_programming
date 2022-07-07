52f193117c20347eb78165a61699aa4fe1ec149a
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-07 20:26:30 +0000

    Update header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,4 +8,8 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+
 #endif
