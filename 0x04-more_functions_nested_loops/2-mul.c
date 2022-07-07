152de8fb1e689da0b998a0ee6800741eb1c5858e
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-07 13:31:59 +0000

    Add 2-mul.c file and add function prototype to the file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
