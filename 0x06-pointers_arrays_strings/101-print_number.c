f8776bab20ce81fbb26275148a417f6fb47081a1
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-17 21:47:58 +0000

    Add correction to task 101 and 102

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	}
+	else
+		m = n;
+
+	if (m / 10)
+		print_number(m / 10);
+
+	_putchar((m % 10) + '0');
+}
