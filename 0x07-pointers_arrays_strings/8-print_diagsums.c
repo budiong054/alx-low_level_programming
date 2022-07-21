b94261e8c0dc4509f0ee9dd90f2509639256e571
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-21 15:06:49 +0000

    Modify output format for printf

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i, j;
-	int sum1, sum2;
+	unsigned int sum1, sum2;
 
 	sum1 = sum2 = 0;
 	for (i = 0; i < size; i++)
@@ -20,11 +20,11 @@ void print_diagsums(int *a, int size)
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				sum1 += *(a + i * size + j);
+				sum1 = sum1 + *(a + i * size + j);
 			if (i + j == size - 1)
 				sum2 += *(a + i * size + j);
 		}
 	}
-	printf("%u, %u\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 
 }
