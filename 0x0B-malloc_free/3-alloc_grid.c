3671024a232f7b1538c29ae5604eb8ed9d61ff51
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-26 21:35:07 +0000

    Add a conditional to check malloc fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,12 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			free(arr);
+			for (j = 0; j <= i; j++)
+				free(arr[j]);
 			return (NULL);
+		}
 	}
 
 	for (j = 0; j < height; j++)
