ac3680a5bb140bd8826767dd4db4aa024a58aaf1
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-28 14:46:02 +0000

    Set memory in task 2 to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+
+char *_memset(char *s, char b, unsigned int n);
+
 /**
  * _calloc - allocates memory for an array, using malloc
  * @nmemb: number of element
@@ -11,18 +14,39 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *s;
+	int n;
 
-	s = 0;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	s = malloc(nmemb * size);
+	n = nmemb * size;
+	s = malloc(n);
 	if (s == NULL)
 	{
 		free(s);
 		return (NULL);
 	}
 
+	_memset(s, 0, n);
+
 	return (s);
 
+}
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed
+ * to by s with the constant byte b
+ * @s: pointer to the string
+ * @b: constant byte
+ * @n: number of byte
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+	return (s);
 }
