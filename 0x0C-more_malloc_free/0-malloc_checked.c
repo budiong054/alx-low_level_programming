c6b9536dacf70736d98f638db7c240a2673eb3e0
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-28 12:59:56 +0000

    Add solution to task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked  - allocates memory using malloc
+ * @b: number of byte
+ * Return: nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+}
