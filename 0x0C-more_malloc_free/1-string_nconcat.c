57d49edac19a37b4ff4d14f0166c3982e367fc07
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-28 13:54:05 +0000

    Add a condition to check NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s3;
 	unsigned int i, len1, len2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
