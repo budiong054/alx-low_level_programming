f437ef2df13e6b4d39cdb4e3e7259765a4f0c96a
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-07-30 11:29:19 +0000

    Add correction to the advance

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,12 +11,31 @@
 
 int main(int argc, char *argv[])
 {
+	int coins[] = {25, 10, 5, 2, 1};
+	int i, cent, num;
+	
 	if (argc - 1 != 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) < 0)
-	       printf("0\n");	
+
+	cent = atoi(argv[1]);
+	if (cent < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	
+	num = 0;
+	for (i = 0; i < 5 && cent >= 0; i++)
+	{
+		while (cent >= coins[i])
+		{
+			num++;
+			cent -= coins[i];
+		}
+	}
+	printf("%d\n", num);
 	return (0);
 }
