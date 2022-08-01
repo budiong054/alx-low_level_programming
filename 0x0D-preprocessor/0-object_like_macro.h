edc909bec020186421f200357d7d8fc263157f0d
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-01 16:46:48 +0000

    Add solution to task 0 to 4

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef OBJECTMACRO_H
+#define OBJECTMACRO_H
+
+#define SIZE 1024
+
+#endif /* OBJECTMARCO_H */
---
+++ 0x0D-preprocessor/3-function_like_marco.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif /* FUNCTION_H */
