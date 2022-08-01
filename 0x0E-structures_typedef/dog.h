e96cdade9f552ff14d1e1fb381acb26822cf6c94
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-01 22:44:37 +0000

    Update dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * age: age of the dog
+ * owner: owner of the dog
+ *
+ * Description: This a structure of a dog
+ */
+
 struct dog
 {
 	char *name;
