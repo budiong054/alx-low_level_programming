f8e30ed55e08ae850f03316e980491479f8965d8
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-01 23:15:40 +0000

    Add the stdlib

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -11,7 +11,9 @@
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-{`
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
