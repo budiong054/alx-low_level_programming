76799c3782d57e038d460c880b1690874c744eec
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-02 20:07:07 +0000

    Add correction to task 4 and 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
@@ -14,12 +16,65 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *cname, *cowner;
+	int len1, len2;
 
 	d  = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
-	d->owner = owner;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	cname = malloc(sizeof(char) * (len1 + 1));
+	if (cname == NULL)
+	{
+		free(cname);
+		return (NULL);
+	}
+	cowner = malloc(sizeof(char) * (len2 + 1));
+	if (cowner == NULL)
+	{
+		free(cowner);
+		return (NULL);
+	}
+	d->name = _strcpy(cname, name);
+	d->owner = _strcpy(cowner, owner);
 	d->age = age;
 	return (d);
+}
+
+/**
+ * _strlen - computes the length of a string
+ * @str: pointer to the string
+ * Return: length of a string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * _strcpy - copies string from src to dest
+ * @src: source
+ * @dest: destination
+ * Return: a pointer to the string destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
