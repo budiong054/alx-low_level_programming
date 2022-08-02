c7b2bba415d1f79715b4c31ba20498ef70bf8c7c
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-02 00:07:16 +0000

    Add solution to task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: a pointer to the newly created structure
+ * return NULL if thr funtion fails
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the dog
+ * @d: pointer to the struct to be free
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+}
