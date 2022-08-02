7490129b2c932deba66d7d6457fd687442f01792
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-02 12:25:40 +0000

    Modify header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,6 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-
 struct dog
 {
 	char *name;
@@ -15,6 +14,10 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - Typedef for unsigned int
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
