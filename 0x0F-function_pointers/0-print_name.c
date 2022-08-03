1af04cd041cd7aca5d6ffcfca79aa77024da2583
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-03 22:24:37 +0000

    Add a condition to avoid segmentation fault

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,6 +13,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (!array || !size || !action)
+		return;
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
