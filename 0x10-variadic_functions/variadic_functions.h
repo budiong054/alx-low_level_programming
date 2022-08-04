ec34d90b43e2e9e1fa83b53425fee9833b327c83
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-04 15:23:23 +0000

    Add a header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
