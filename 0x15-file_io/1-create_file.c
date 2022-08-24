d91415ff1eaf4ade5c0d2489066abeae1e10f570
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-08-24 06:52:09 +0000

    Add a flag to truncate it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
