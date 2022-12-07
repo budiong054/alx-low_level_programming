895650ef659a3c5caf3cb26fbaf2f1866fe38671
Blessing Godwin Udiong <udiongblessing@gmail.com>
2022-12-07 12:33:37 +0000

    Add README and solution to task 0 - 4

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""The ``5-island_perimeter`` module
+"""
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in `grid`
+
+    Args:
+        grid(list): is a list of list
+    """
+
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,13 @@
+# 0x1C. C - Makefiles
+
+## Tasks
+---
+### 0. make -f 0-Makefile
+
+### 1. make -f 1-Makefile
+
+### 2. make -f 2-Makefile
+
+### 3. make -f 3-Makefile
+
+### 4. A complete Makefile
